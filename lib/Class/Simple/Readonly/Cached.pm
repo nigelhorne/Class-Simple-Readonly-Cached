9b731a988aa295591e1bdef83e4bd0902b93e17e
Nigel Horne <nigel.horne@nasa.gov>
2020-12-17 14:38:28 -0500

    Release 0.06

--- lib/Class/Simple/Readonly/Cached.pm
+++ lib/Class/Simple/Readonly/Cached.pm
@@ -13,11 +13,11 @@ Class::Simple::Readonly::Cached - cache messages to an object
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
