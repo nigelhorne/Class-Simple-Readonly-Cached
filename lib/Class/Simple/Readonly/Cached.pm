467341ab85e24a246a63fc317014c9ee89ff4416
Nigel Horne <njh@bandsman.co.uk>
2021-01-08 18:53:31 -0500

    Release 0.07

--- lib/Class/Simple/Readonly/Cached.pm
+++ lib/Class/Simple/Readonly/Cached.pm
@@ -13,11 +13,11 @@ Class::Simple::Readonly::Cached - cache messages to an object
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 =head1 SYNOPSIS
 
